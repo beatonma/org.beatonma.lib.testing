f451d9e9a179853a8b8b9fe891a9bac117e0be88
Michael Beaton <beatonma@gmail.com>
2018-07-26 16:39:09 +0100

    Added optional intentBlock to testRule for simple passing of extras to the activity intent

--- src/main/java/org/beatonma/lib/testing/kotlin/extensions/Rules.kt
+++ src/main/java/org/beatonma/lib/testing/kotlin/extensions/Rules.kt
@@ -3,6 +3,8 @@
 package org.beatonma.lib.testing.kotlin.extensions
 
 import android.app.Activity
+import android.content.Intent
+import androidx.test.InstrumentationRegistry
 import androidx.test.rule.ActivityTestRule
 import kotlin.reflect.KClass
 
@@ -14,10 +16,21 @@ val <T : Activity> KClass<T>.testRule: ActivityTestRule<T>
 
 /**
  * Convenience for ActivityTestRule(javaClass, initialTouchMode, launchActivity)
+ * You may also alter the intent that is used to launch the activity by providing an intentBlock
  */
 fun <T : Activity> KClass<T>.testRule(
         initialTouchMode: Boolean = true,
-        launchActivity: Boolean = true
+        launchActivity: Boolean = true,
+        intentBlock: ((Intent) -> Unit)? = null
 ): ActivityTestRule<T> {
-    return ActivityTestRule(java, initialTouchMode, launchActivity)
+    return if (intentBlock == null) ActivityTestRule(java, initialTouchMode, launchActivity)
+    else {
+        object: ActivityTestRule<T>(java, initialTouchMode, launchActivity) {
+            override fun getActivityIntent(): Intent {
+                return Intent(InstrumentationRegistry.getContext(), this@testRule.java).apply {
+                    intentBlock(this)
+                }
+            }
+        }
+    }
 }
