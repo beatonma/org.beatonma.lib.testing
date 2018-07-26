81c91220665139d4d91285520007bffc36e3192c
Michael Beaton <beatonma@gmail.com>
2018-07-26 14:31:52 +0100

    Custom click actions to allow tapping at difference locations on a View

---
+++ src/main/java/org/beatonma/lib/testing/espresso/Click.kt
@@ -0,0 +1,42 @@
+@file:JvmName("Click")
+
+package org.beatonma.lib.testing.espresso
+
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.GeneralClickAction
+import androidx.test.espresso.action.Press
+import androidx.test.espresso.action.Tap
+import androidx.test.espresso.action.ViewActions
+
+/**
+ * Tap at a custom point on the [View]
+ */
+fun click(x: Float = .5F, y: Float = .5F): ViewAction {
+    return ViewActions.actionWithAssertions(
+            GeneralClickAction(
+                    Tap.SINGLE,
+                    getCoordinatesProvider(x, y, allowOutOfBounds = true),
+                    Press.FINGER, 0, 0))
+}
+
+/**
+ * Long press at a custom point on the [View]
+ */
+fun longClick(x: Float = .5F, y: Float = .5F): ViewAction {
+    return ViewActions.actionWithAssertions(
+            GeneralClickAction(
+                    Tap.LONG,
+                    getCoordinatesProvider(x, y, allowOutOfBounds = true),
+                    Press.FINGER, 0, 0))
+}
+
+/**
+ * Double tap at a custom point on the [View]
+ */
+fun doubleClick(x: Float = .5F, y: Float = .5F): ViewAction {
+    return ViewActions.actionWithAssertions(
+            GeneralClickAction(
+                    Tap.DOUBLE,
+                    getCoordinatesProvider(x, y, allowOutOfBounds = true),
+                    Press.FINGER, 0, 0))
+}
