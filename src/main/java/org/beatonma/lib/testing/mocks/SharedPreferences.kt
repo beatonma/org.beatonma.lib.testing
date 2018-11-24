1f8287dd3f625753ded57d05406610a99d155507
Michael Beaton <beatonma@gmail.com>
2018-11-24 14:06:56 +0000

    Added reusable mock implementation of SharedPreferences and SharedPreferences.Editor.

---
+++ src/main/java/org/beatonma/lib/testing/mocks/SharedPreferences.kt
@@ -0,0 +1,34 @@
+@file:JvmName("SharedPreferences")
+
+package org.beatonma.lib.testing.mocks
+
+import android.content.SharedPreferences
+import org.beatonma.lib.testing.kotlin.extensions.mock
+import org.beatonma.lib.testing.kotlin.extensions.whenever
+import org.mockito.Matchers
+
+fun mockedSharedPreferences(
+        intValue: Int = 31,
+        booleanValue: Boolean = true,
+        stringValue: String = "some_string"
+): SharedPreferences {
+    return mock<SharedPreferences>().apply {
+        whenever(getBoolean(Matchers.anyString(), Matchers.anyBoolean())).then { booleanValue }
+        whenever(getInt(Matchers.anyString(), Matchers.anyInt())).then { intValue }
+        whenever(getString(Matchers.anyString(), Matchers.anyString())).then { stringValue }
+    }
+}
+
+fun mockedSharedPreferencesEditor(fakePrefs: HashMap<String, Any?>): SharedPreferences.Editor {
+    return mock<SharedPreferences.Editor>().apply {
+        whenever(putBoolean(Matchers.anyString(), Matchers.anyBoolean())).then {
+            fakePrefs.put(it.arguments[0] as String, it.arguments[1])
+        }
+        whenever(putString(Matchers.anyString(), Matchers.anyString())).then {
+            fakePrefs.put(it.arguments[0] as String, it.arguments[1])
+        }
+        whenever(putInt(Matchers.anyString(), Matchers.anyInt())).then {
+            fakePrefs.put(it.arguments[0] as String, it.arguments[1])
+        }
+    }
+}
